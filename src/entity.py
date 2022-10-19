a79862ddd93c5eb50a641d24954c8bc243250e78
Stephen Czekalski <stephencze243@gmail.com>
2022-10-19 09:21:12 -0400

    Added entity manager and deltatime to entity render and logic functions.

--- src/entity.py
+++ src/entity.py
@@ -2,6 +2,22 @@ import sys, pygame
 from abc import ABC, abstractmethod
 
 
+class EntityManager():
+
+  def __init__(self):
+    self._entities = []
+
+  def add(self, entity):
+    self._entities.append(entity)
+
+  @property
+  def entities(self):
+    return self._entities
+
+  @entities.setter
+  def entities(self, entities):
+    self._entities = entities
+
 """
 The Entity abstract base class provides the interface that 
 all screens shouold implement.
@@ -12,7 +28,11 @@ class Entity(ABC):
     pass
 
   @abstractmethod
-  def render(self):
+  def logic(self, deltatime):
+    pass
+
+  @abstractmethod
+  def render(self, deltatime):
     pass
 
   @abstractmethod
@@ -20,12 +40,29 @@ class Entity(ABC):
     pass
 
 
+class DropDownEntity(Entity):
+
+  def __init__(self):
+    pass
+
+  def logic(self, deltatime):
+    pass
+
+  def render(self, deltatime):
+    pass
+
+  def destroy(self):
+    pass
+
 class TimerEntity(Entity):
   
   def __init__(self):
     pass
 
-  def render(self):
+  def logic(self, deltatime):
+    pass
+
+  def render(self, deltatime):
     pass
 
   def destroy(self):
--- src/screen.py
+++ src/screen.py
@@ -1,5 +1,6 @@
 import pygame
 from abc import ABC, abstractmethod
+from entity import EntityManager
 from entity import TimerEntity
 
 """
@@ -10,6 +11,10 @@ class Screen(ABC):
 
   def __init__(self, game):
     self._game = game
+
+  @abstractmethod
+  def logic(self, deltatime):
+    pass
 
   @abstractmethod
   def render(self, deltatime):
@@ -29,18 +34,23 @@ class GameScreen(Screen):
     Screen.__init__(self, game)
 
     # Entities
+    self._entity_manager = EntityManager()
     self._entity_timer = TimerEntity()
+
+    self._entity_manager.add(self._entity_timer)
 
   def logic(self, deltatime):
     for event in pygame.event.get():
       self._handle_quit_event(event)
       self._handle_resize_event(event)  
 
+    for entity in self._entity_manager.entities:
+      entity.logic(deltatime)
+
 
   def _handle_quit_event(self, event):
     if event.type == pygame.QUIT:
       self.destroy()
-      self
 
   def _handle_resize_event(self, event):
     if event.type == pygame.VIDEORESIZE:
@@ -48,9 +58,14 @@ class GameScreen(Screen):
 
   def render(self, deltatime):
     self._game._display.fill((255, 255, 255))
-    self._entity_timer.render()
+    
+    for entity in self._entity_manager.entities:
+      entity.render(deltatime)
 
     pygame.display.flip()
 
   def destroy(self):
     self._game._should_destroy = True
+
+    for entity in self._entity_manager.entities:
+      entity.destroy()
