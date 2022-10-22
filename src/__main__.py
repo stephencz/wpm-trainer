611acdcbfb7b558bd32278262a589f4bea8e5188
Stephen Czekalski <stephencze243@gmail.com>
2022-10-22 12:47:34 -0400

    Fixed problem with application not using icon in task bar on Windows 11.

--- src/__main__.py
+++ src/__main__.py
@@ -1,5 +1,11 @@
 import sys
 
+# Makes icon work in taskbar on Windows 10/11
+import ctypes
+myappid = "com.stephencz.wpm.trainer.1.0.0"
+ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
+
+# Configure matplotlib for embedded use
 import matplotlib
 matplotlib.use("Qt5Agg") #Needed for embedding matplotlib in Qt
 
@@ -11,6 +17,8 @@ from gui import MainWindow
 
 
 def main():
+
+
   app = QApplication(sys.argv)
 
   window = MainWindow()
